2cd6b0df148887b2d4f4a387b64759fc0fa0c9d2
Mahmoud <mamah1184@uis.no>
2026-06-18 23:31:49 +0200

    Initial commit: added backup script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SOURCE_DIR="$HOME/devops_project/data"
+BACKUP_DIR="$HOME/devops_project/backups"
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="$BACKUP_DIR/backup_$DATE.tar.gz"
+
+mkdir -p "$BACKUP_DIR"
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+echo "Backup completed successfully at $BACKUP_FILE"
+
